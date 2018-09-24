74659fa6ee0f303dfc6e4c5c32640ab6f8e06e96
Nick Quinn <n.quinn@optoma.co.uk>
2018-09-24 16:25:42 +0100

    Initial commit

--- YoutubeDl/Form1.Designer.cs
+++ YoutubeDl/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtVideoUrl = new System.Windows.Forms.TextBox();
+            this.chkAudioOnly = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(495, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtVideoUrl
+            // 
+            this.txtVideoUrl.Location = new System.Drawing.Point(12, 12);
+            this.txtVideoUrl.Name = "txtVideoUrl";
+            this.txtVideoUrl.Size = new System.Drawing.Size(347, 20);
+            this.txtVideoUrl.TabIndex = 1;
+            // 
+            // chkAudioOnly
+            // 
+            this.chkAudioOnly.AutoSize = true;
+            this.chkAudioOnly.Location = new System.Drawing.Point(366, 12);
+            this.chkAudioOnly.Name = "chkAudioOnly";
+            this.chkAudioOnly.Size = new System.Drawing.Size(94, 17);
+            this.chkAudioOnly.TabIndex = 2;
+            this.chkAudioOnly.Text = "Only get audio";
+            this.chkAudioOnly.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(519, 85);
+            this.Controls.Add(this.chkAudioOnly);
+            this.Controls.Add(this.txtVideoUrl);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Phil\'s YouTube downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtVideoUrl;
+        private System.Windows.Forms.CheckBox chkAudioOnly;
     }
 }
 
--- YoutubeDl/Form1.cs
+++ YoutubeDl/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NYoutubeDL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,45 @@ namespace YoutubeDl
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var youtubeDl = new YoutubeDL();
+            youtubeDl.Options.PostProcessingOptions.FfmpegLocation = "ffmpeg.exe";
+            youtubeDl.Options.PostProcessingOptions.PreferFfmpeg = true;
+
+            if (chkAudioOnly.Checked)
+            {
+                youtubeDl.Options.FilesystemOptions.Output = $"downloads/youtubedl.%(ext)s";
+                youtubeDl.Options.PostProcessingOptions.AudioFormat = NYoutubeDL.Helpers.Enums.AudioFormat.mp3;
+                youtubeDl.Options.PostProcessingOptions.KeepVideo = false;
+                youtubeDl.Options.PostProcessingOptions.ExtractAudio = true;
+            }
+            else
+            {
+                youtubeDl.Options.FilesystemOptions.Output = $"downloads/youtubedl.mp4";
+                youtubeDl.Options.VideoFormatOptions.Format = NYoutubeDL.Helpers.Enums.VideoFormat.mp4;
+                youtubeDl.Options.PostProcessingOptions.KeepVideo = true;
+            }
+            
+            youtubeDl.VideoUrl = txtVideoUrl.Text;
+            // Optional, required if binary is not in $PATH
+            youtubeDl.YoutubeDlPath = "youtube-dl.exe";
+
+            // Prepare the download (in case you need to validate the command before starting the download)
+            string commandToRun = await youtubeDl.PrepareDownloadAsync();
+
+            // Just let it run
+            txtVideoUrl.Enabled = false;
+            button1.Enabled = false;
+            button1.Text = "Downloading...";
+            await youtubeDl.DownloadAsync();
+
+            txtVideoUrl.Enabled = true;
+            button1.Enabled = true;
+
+            MessageBox.Show("Completed");
         }
     }
 }
