63d11b00a61df88eaf76584dec39ea01e9186e87
Nick Quinn <n.quinn@optoma.co.uk>
2018-09-25 10:24:55 +0100

    Progress indicator + formalised lib output

--- YoutubeDl/Form1.Designer.cs
+++ YoutubeDl/Form1.Designer.cs
@@ -28,14 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.txtVideoUrl = new System.Windows.Forms.TextBox();
             this.chkAudioOnly = new System.Windows.Forms.CheckBox();
+            this.mainProgressBar = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.doofersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitYourJibberJabberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtSaveFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.txtTerminal = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 48);
+            this.button1.Location = new System.Drawing.Point(12, 129);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(495, 23);
             this.button1.TabIndex = 0;
@@ -45,31 +57,121 @@
             // 
             // txtVideoUrl
             // 
-            this.txtVideoUrl.Location = new System.Drawing.Point(12, 12);
+            this.txtVideoUrl.Location = new System.Drawing.Point(102, 69);
             this.txtVideoUrl.Name = "txtVideoUrl";
-            this.txtVideoUrl.Size = new System.Drawing.Size(347, 20);
+            this.txtVideoUrl.Size = new System.Drawing.Size(305, 20);
             this.txtVideoUrl.TabIndex = 1;
             // 
             // chkAudioOnly
             // 
             this.chkAudioOnly.AutoSize = true;
-            this.chkAudioOnly.Location = new System.Drawing.Point(366, 12);
+            this.chkAudioOnly.Location = new System.Drawing.Point(413, 72);
             this.chkAudioOnly.Name = "chkAudioOnly";
-            this.chkAudioOnly.Size = new System.Drawing.Size(94, 17);
+            this.chkAudioOnly.Size = new System.Drawing.Size(82, 17);
             this.chkAudioOnly.TabIndex = 2;
-            this.chkAudioOnly.Text = "Only get audio";
+            this.chkAudioOnly.Text = "Create MP3";
             this.chkAudioOnly.UseVisualStyleBackColor = true;
+            // 
+            // mainProgressBar
+            // 
+            this.mainProgressBar.Location = new System.Drawing.Point(12, 158);
+            this.mainProgressBar.Name = "mainProgressBar";
+            this.mainProgressBar.Size = new System.Drawing.Size(495, 22);
+            this.mainProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.mainProgressBar.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.doofersToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(519, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // doofersToolStripMenuItem
+            // 
+            this.doofersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitYourJibberJabberToolStripMenuItem});
+            this.doofersToolStripMenuItem.Name = "doofersToolStripMenuItem";
+            this.doofersToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.doofersToolStripMenuItem.Text = "&Doofers";
+            // 
+            // quitYourJibberJabberToolStripMenuItem
+            // 
+            this.quitYourJibberJabberToolStripMenuItem.Name = "quitYourJibberJabberToolStripMenuItem";
+            this.quitYourJibberJabberToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.quitYourJibberJabberToolStripMenuItem.Text = "&Quit your jibber jabber";
+            this.quitYourJibberJabberToolStripMenuItem.Click += new System.EventHandler(this.quitYourJibberJabberToolStripMenuItem_Click);
+            // 
+            // txtSaveFolder
+            // 
+            this.txtSaveFolder.Location = new System.Drawing.Point(102, 27);
+            this.txtSaveFolder.Name = "txtSaveFolder";
+            this.txtSaveFolder.ReadOnly = true;
+            this.txtSaveFolder.Size = new System.Drawing.Size(305, 20);
+            this.txtSaveFolder.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Save folder:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "YouTube URL:";
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(414, 27);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 8;
+            this.btnChange.Text = "Change";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // txtTerminal
+            // 
+            this.txtTerminal.Location = new System.Drawing.Point(12, 186);
+            this.txtTerminal.Multiline = true;
+            this.txtTerminal.Name = "txtTerminal";
+            this.txtTerminal.ReadOnly = true;
+            this.txtTerminal.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtTerminal.Size = new System.Drawing.Size(495, 161);
+            this.txtTerminal.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 85);
+            this.ClientSize = new System.Drawing.Size(519, 359);
+            this.Controls.Add(this.txtTerminal);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSaveFolder);
+            this.Controls.Add(this.mainProgressBar);
             this.Controls.Add(this.chkAudioOnly);
             this.Controls.Add(this.txtVideoUrl);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Phil\'s YouTube downloader";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +182,16 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtVideoUrl;
         private System.Windows.Forms.CheckBox chkAudioOnly;
+        private System.Windows.Forms.ProgressBar mainProgressBar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem doofersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitYourJibberJabberToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtSaveFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.TextBox txtTerminal;
     }
 }
 
--- YoutubeDl/Form1.cs
+++ YoutubeDl/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,45 +17,93 @@ namespace YoutubeDl
         public Form1()
         {
             InitializeComponent();
+
+            string homePath = (Environment.OSVersion.Platform == PlatformID.Unix ||
+                   Environment.OSVersion.Platform == PlatformID.MacOSX)
+    ? Environment.GetEnvironmentVariable("HOME")
+    : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+
+            txtSaveFolder.Text = homePath;
+        }
+
+        private static string CleanFileName(string fileName)
+        {
+            return Path.GetInvalidFileNameChars().Aggregate(fileName, (current, c) => current.Replace(c.ToString(), string.Empty));
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
+            mainProgressBar.Visible = true;
+            txtVideoUrl.Enabled = false;
+            button1.Enabled = false;
+            btnChange.Enabled = false;
+            menuStrip1.Enabled = false;
+
             var youtubeDl = new YoutubeDL();
-            youtubeDl.Options.PostProcessingOptions.FfmpegLocation = "ffmpeg.exe";
+
+            // Optional, required if binary is not in $PATH
+            youtubeDl.YoutubeDlPath = "lib/youtube-dl.exe";
+            youtubeDl.VideoUrl = txtVideoUrl.Text;
+
+            youtubeDl.Options.PostProcessingOptions.FfmpegLocation = "lib/ffmpeg.exe";
             youtubeDl.Options.PostProcessingOptions.PreferFfmpeg = true;
+
+            var tmpFn = CleanFileName(DateTime.Now.ToString("ddMMyyyy hhmmss"));
 
             if (chkAudioOnly.Checked)
             {
-                youtubeDl.Options.FilesystemOptions.Output = $"downloads/youtubedl.%(ext)s";
+                youtubeDl.Options.FilesystemOptions.Output = $"{txtSaveFolder.Text}/{tmpFn}.%(ext)s";
                 youtubeDl.Options.PostProcessingOptions.AudioFormat = NYoutubeDL.Helpers.Enums.AudioFormat.mp3;
                 youtubeDl.Options.PostProcessingOptions.KeepVideo = false;
                 youtubeDl.Options.PostProcessingOptions.ExtractAudio = true;
             }
             else
             {
-                youtubeDl.Options.FilesystemOptions.Output = $"downloads/youtubedl.mp4";
+                youtubeDl.Options.FilesystemOptions.Output = $"{txtSaveFolder.Text}/{tmpFn}.mp4";
                 youtubeDl.Options.VideoFormatOptions.Format = NYoutubeDL.Helpers.Enums.VideoFormat.mp4;
                 youtubeDl.Options.PostProcessingOptions.KeepVideo = true;
             }
-            
-            youtubeDl.VideoUrl = txtVideoUrl.Text;
-            // Optional, required if binary is not in $PATH
-            youtubeDl.YoutubeDlPath = "youtube-dl.exe";
 
-            // Prepare the download (in case you need to validate the command before starting the download)
             string commandToRun = await youtubeDl.PrepareDownloadAsync();
 
+            youtubeDl.StandardOutputEvent += (sdr, output) =>
+            {
+                Console.WriteLine(output);
+                this.txtTerminal.Invoke((Action)delegate { this.txtTerminal.AppendText(Environment.NewLine + output); });
+            };
+            youtubeDl.StandardErrorEvent += (sdr, errorOutput) => Console.WriteLine(errorOutput);
+
+            // Prepare the download (in case you need to validate the command before starting the download)
+            //string commandToRun = await youtubeDl.PrepareDownloadAsync();
+
+
+            youtubeDl.Info.PropertyChanged += delegate {
+                this.mainProgressBar.Invoke((Action)delegate { this.mainProgressBar.Value = youtubeDl.Info.VideoProgress; });
+                //mainProgressBar.Update();
+                //var val = youtubeDl.Info.VideoProgress;
+            };
+
             // Just let it run
-            txtVideoUrl.Enabled = false;
-            button1.Enabled = false;
-            button1.Text = "Downloading...";
             await youtubeDl.DownloadAsync();
 
             txtVideoUrl.Enabled = true;
             button1.Enabled = true;
+            mainProgressBar.Visible = false;
+            btnChange.Enabled = true;
+            menuStrip1.Enabled = true;
+        }
 
-            MessageBox.Show("Completed");
+        private void quitYourJibberJabberToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                txtSaveFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
         }
     }
 }
